3ae303e8c0c6b0ccad1617c8b36366c484a5ad04
sriwaugh <sriwaugh0987@gmail.com>
2024-03-18 21:22:18 +0530

    String_methods

---
+++ String_methods.java
@@ -0,0 +1,15 @@
+package java_program;
+import  java.util.*;
+public class String_methods {
+    public static void main(String[] args) {
+            String sen="The sky is blues";
+            System.out.println(Arrays.toString(sen.split(" ")));
+            String[] sent=sen.split(" ");
+            System.out.println(sent[3].length());
+            String sente=sent[3];
+            System.out.println(sente);
+            System.out.println(sente.charAt(1));
+
+        }
+    }
+
