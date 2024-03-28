38761ca8102e9ed39b98c9f51d28760ee7acd8f1
sriwaugh <sriwaugh0987@gmail.com>
2024-03-28 18:18:44 +0530

    Fibonnaci_using_recursion

---
+++ Fibonnaci_using_recursion.java
@@ -0,0 +1,28 @@
+package java_program;
+import java.util.Scanner;
+public class Fibonnaci_using_recursion {
+    public static  int fib(int a)
+        {
+
+            if(a==0||a==1)
+            {
+                return a;
+            }
+
+            else {
+
+                a=fib(a-1)+fib(a-2);
+                return a;
+            }
+        }
+
+        public static void main(String[] args) {
+            int a;
+            Scanner s=new Scanner(System.in);
+            a=s.nextInt();
+            int res=fib(a);
+            System.out.println(res);
+        }
+
+    }
+
