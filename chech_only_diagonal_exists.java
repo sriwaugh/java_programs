399e49a0bec856bb8b1bcb9d1efd49a63f529a46
sriwaugh <sriwaugh0987@gmail.com>
2024-04-03 17:58:26 +0530

    Print_the_left_diagonal_of_matrix

---
+++ chech_only_diagonal_exists.java
@@ -0,0 +1,48 @@
+package java_program;
+import java.util.Scanner;
+public class chech_only_diagonal_exists {
+        public static void main(String[] args) {
+            int n,m;
+            Scanner s= new Scanner(System.in);
+            System.out.println("Enter the row of the matrix");
+            n=s.nextInt();
+            System.out.println("enter the column of the matrix");
+            m=s.nextInt();
+            int arr[][]=new int[n][m];
+            System.out.println("Enter the matrix element");
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    arr[i][j]=s.nextInt();
+                }
+            }
+            System.out.println("The matrix element are");
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println("The  daigonal value in matrix");
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    if(i==j)
+                    {
+                        System.out.print(arr[i][j]+" ");
+                    }
+                    else{
+                        int y=arr[i][j]=0;
+                        System.out.print(y+" ");
+                    }
+
+                }
+                System.out.println();
+            }
+        }
+    }
+
