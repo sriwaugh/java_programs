4da2fae7161dc3f75058e99f65c0d047489a28a2
sriwaugh <sriwaugh0987@gmail.com>
2024-04-04 15:07:18 +0530

    square

---
+++ abstract program/sqr.java
@@ -0,0 +1,14 @@
+package abstract_program;
+class sqr extends Shape {
+    public sqr(double i)
+    {
+        s1=i;
+    }
+    public double area()
+    {
+        return s1*s1;
+    }
+    public double perimeter()
+    {
+        return 4*s1;
+    }}
