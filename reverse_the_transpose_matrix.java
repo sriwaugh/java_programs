530af4e949ae6e1d6a91637c5987d49d17cd4331
sriwaugh <sriwaugh0987@gmail.com>
2024-04-08 11:10:39 +0530

    reverse_the_transpose_matrix

---
+++ reverse_the_transpose_matrix.java
@@ -0,0 +1,51 @@
+package java_program;
+import java.util.Scanner;
+public class reverse_the_transpose_matrix {
+        public static void main(String[] args) {
+            int n,m,i,j;
+            Scanner s=new Scanner(System.in);
+            System.out.println("Enter the row of the matrix");
+            n=s.nextInt();
+            System.out.println("Enter the coloumn of the matrix");
+            m=s.nextInt();
+            int a[][]=new int[n][m];
+            int arr1[][]=new int[n][m];
+            System.out.println("Enter the matrix element");
+            for(i=0;i<n;i++){
+                for(j=0;j<m;j++)
+                {
+                    a[i][j]=s.nextInt();
+                }
+            }
+            System.out.println("The matrix is");
+            for ( i = 0; i < n; i++) {
+                for (j = 0;j<m; j++) {
+                    System.out.print(a[i][j]+" ");
+
+                }
+                System.out.println();
+            }
+            for ( i = 0; i < n; i++) {
+                for (j = 0;j<m; j++) {
+                    arr1[i][j]=a[j][i];
+
+                }
+
+            }
+            System.out.println("The transpose matix is");
+            for ( i = 0; i < n; i++) {
+                for (j = 0;j<m; j++) {
+                    System.out.print(arr1[i][j]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println("The reverse of the transpose matix is");
+            for ( i = 0; i < n; i++) {
+                for (j = n-1;j>=0; j--) {
+                    System.out.print(arr1[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+
