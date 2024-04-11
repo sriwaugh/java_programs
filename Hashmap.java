551e1037a7fbbb6a9ebb2c4a3750df3d8b5aafa2
sriwaugh <sriwaugh0987@gmail.com>
2024-04-11 19:44:13 +0530

    Hashmap

---
+++ Hashmap.java
@@ -0,0 +1,19 @@
+package java_program;
+import java.util.HashMap;
+public class Hashmap {
+        public static void main(String[] args) {
+            HashMap<String,String> c=new HashMap<>();
+            c.put("tamilnadu","Chennai");
+            c.put("kerala","trivandram");
+            c.put("karanataka","bangalore");
+            c.put("andra ","hyderbad");
+            System.out.println(c.get("kerala"));
+            c.remove("kerala");
+            System.out.println(c);
+            c.clear();
+            System.out.println(c);
+            System.out.println(c.size());
+
+        }
+    }
+
