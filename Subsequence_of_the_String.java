3318775e9f255b05840522201438089411f424e7
sriwaugh <sriwaugh0987@gmail.com>
2024-04-16 19:02:27 +0530

    Subsequence of string

---
+++ Subsequence_of_the_String.java
@@ -0,0 +1,24 @@
+package java_program;
+
+public class Subsequence_of_the_String {
+        public static void main(String[] args) {
+            String a="abc";
+            String b="afbgco";
+            int count=0;
+            char str[]=a.toCharArray();
+            char str1[]=b.toCharArray();
+
+            for (int i = 0; i < str.length; i++) {
+                for (int j = 0; j < str1.length; j++) {
+                    if(str[i]==str[j])
+                    {
+                        if(i!=j)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
