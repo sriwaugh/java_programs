ba78161df8d6c77e215364764dd3dbbc75ccec7c
sriwaugh <154859609+sriwaugh@users.noreply.github.com>
2024-04-17 18:55:57 +0530

    Update Target_value.java

--- Target_value.java
+++ Target_value.java
@@ -1,4 +1,30 @@
 package java_program;
 
 public class Target_value {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 7, 8, 9, 90, 6};
+        int n = arr.length;
+        int j = 0;
+        int t = 99;
+        for (int i = 0; i < n; i++)
+        {
+            loop:
+            if (j == n - 1)
+            {
+                j = 0;
+                continue;
+            }
+            else
+            {
+                j++;
+                if (arr[i] + arr[j]==t)
+                {
+                    System.out.println(i + " " + j);
+                }
+                break loop;
+            }
+        }
+    }
 }
+
+
