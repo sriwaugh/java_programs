83fd3efadd54c3acb73f660c83821c6265c06ab1
sriwaugh <sriwaugh0987@gmail.com>
2024-04-18 17:21:32 +0530

    Happy number

---
+++ Happy_number.java
@@ -0,0 +1,32 @@
+package java_program;
+import java.util.HashSet;
+public class Happy_number {
+        public static void main(String[] args) {
+            int a=25,sum=0;
+            HashSet<Integer> n=new HashSet<>();
+            while(true){
+                sum=0;
+                while (a>0)
+                {
+                    int rem=a%10;
+                    sum+=rem*rem;
+                    a=a/10;
+                }
+                System.out.println(sum);
+                if(sum==1)
+                {
+                    System.out.println("happy number");
+                    System.exit(0);
+                }
+                a=sum;
+                if(n.contains(a))
+                {
+                    System.out.println("Not happy number");
+                    System.exit(0);
+                }
+                n.add(a);
+            }
+        }
+    }
+
+
--- Subsequence_of_the_String.java
+++ Subsequence_of_the_String.java
@@ -20,5 +20,5 @@ public class Subsequence_of_the_String {
                 }
             }
         }
-    }
+}
 
