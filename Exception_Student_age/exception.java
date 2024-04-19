35392c62e111dbe13e456ce610d66f6ed07921eb
sriwaugh <sriwaugh0987@gmail.com>
2024-04-19 17:29:36 +0530

    Exception

---
+++ Exception_Student_age/exception.java
@@ -0,0 +1,7 @@
+public class exception {
+    public static void main(String[] args)throws Exception {
+        Student s1=new Student();
+        s1.setage(-10);
+        System.out.println(s1.getage());
+    }
+}
