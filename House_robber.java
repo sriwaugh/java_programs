fe07a2bc413b576dfc2ea643bc2d3208e53d1ef7
sriwaugh <sriwaugh0987@gmail.com>
2024-04-25 09:52:55 +0530

    House_robber

---
+++ House_robber.java
@@ -0,0 +1,21 @@
+package java_program;
+import java.util.Scanner;
+public class House_robber {
+        public static void main(String[] args) {
+            int n,sum=0;
+            Scanner s=new Scanner(System.in);
+            n=s.nextInt();
+            int a[]=new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i]=s.nextInt();
+            }
+            for (int i = 0; i <n ; i++) {
+
+                sum+=a[i];
+                i++;
+
+            }
+            System.out.println(sum);
+        }
+    }
+
