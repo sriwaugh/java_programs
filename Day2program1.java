4dd3b6cf48c4ab5b88cb103a953a7a099b0accdf
sriwaugh <sriwaugh0987@gmail.com>
2024-06-18 19:34:34 +0530

    Day2program3

--- Day2program1.java
+++ Day2program1.java
@@ -1,2 +1,55 @@
-package java_program;public class Day2program1 {
-}
+package java_program;
+    class Stack {
+        int len;
+        int stack[];
+        int top;
+
+        Stack(int len) {
+            this.len = len;
+            stack = new int[len];
+            top = -1;
+        }
+
+        public boolean isEmpty() {
+            return (top == -1);
+        }
+
+        public boolean isFull() {
+            return (len - 1 == top);
+        }
+
+        public void push(int data) {
+            if (isFull()) {
+                System.out.println("Stack is Full");
+            } else {
+                stack[++top] = data;
+            }
+        }
+
+        public void pop() {
+            if (isEmpty()) {
+                System.out.println("Stack is Empty");
+            } else {
+                top--;
+            }
+        }
+
+        public int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return stack[top];
+        }
+    }
+
+    public class Day2program1 {
+        public static void main(String[] args) {
+            Stack st = new Stack(1);
+            st.push(0);
+            st.push(10);
+            System.out.println(st.peek());
+        }
+    }
+
+
+
