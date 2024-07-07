3d10870e4e9593c3ad3ad4fbd029ca5fdc87dfd6
sriwaugh <sriwaugh0987@gmail.com>
2024-07-07 19:52:31 +0530

    FindTheTaregetValue

---
+++ FindTheTargetvalue.java
@@ -0,0 +1,28 @@
+package java_program;
+
+public class FindTheTargetvalue {
+    public static int[] twoSum(int[] nums, int target) {
+
+        int a[]=new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            for(int j=i+1;j<nums.length;j++)
+            {
+                if(nums[i]+nums[j]==target)
+                {
+                    a[0]=i;
+                    a[1]=j;
+                }
+            }
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        int a[]=new int[]{1,2,3,4,5,8,7};
+        int b=5;
+        int[] c=twoSum(a,b);
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
+        }
+    }
+}
