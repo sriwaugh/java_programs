0910301d2d55733d23285179fffc56dc29c450b9
sriwaugh <sriwaugh0987@gmail.com>
2024-07-08 20:07:06 +0530

    Validparentheses

---
+++ ValidParentheses.java
@@ -0,0 +1,40 @@
+package java_program;
+import java.util.Stack;
+import java.util.Scanner;
+public class ValidParentheses {
+    public static boolean isValid(String s) {
+        char[] st=s.toCharArray();
+        Stack<Character> a=new Stack();
+        int n=st.length;
+        for (int i = 0; i < n; i++)
+        {
+            if (st[i] == '(') {
+
+                a.push(')');
+            } else if (st[i] == '[') {
+
+                a.push(']');
+            } else if (st[i] == '{') {
+                a.push('}');
+            } else if (a.isEmpty()) {
+                return false;
+            }
+            else{
+                if(!a.isEmpty()&&a.peek().equals(st[i])){
+                    a.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return a.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        String a= s.next();
+        boolean x=isValid(a);
+        System.out.println(x);
+    }
+}
