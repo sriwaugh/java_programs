421c13e87b50aa9ac136ac8b276ebb3e50a6f262
sriwaugh <sriwaugh0987@gmail.com>
2024-07-10 20:34:20 +0530

    SearchInsertPortion

---
+++ SearchInsertPortion.java
@@ -0,0 +1,38 @@
+package java_program;
+
+public class SearchInsertPortion {
+    public static int searchInsert(int[] nums, int target) {
+        int x=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }
+        }
+        int j=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i<nums.length-1)
+                j=i+1;
+            if(nums[i]>target)
+            {
+                return 0;
+            }
+            if(nums[i]<target&&nums[j]>target)
+            {
+                x=i+1;
+                return x;
+            }
+        }
+
+        x=nums.length;
+        return x;
+    }
+
+    public static void main(String[] args) {
+         int[] a=new int[]{1,2,3,5,6,7};
+         int b=4;
+        System.out.println(searchInsert(a,b));
+    }
+}
