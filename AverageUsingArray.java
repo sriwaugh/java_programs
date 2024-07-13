13c98086cc2a7d399c2b7c1364475b7a74b621c4
sriwaugh <sriwaugh0987@gmail.com>
2024-07-13 20:41:31 +0530

    AverageUsingArray

---
+++ AverageUsingArray.java
@@ -0,0 +1,16 @@
+package java_program;
+
+public class AverageUsingArray {
+    public static void main(String[] args) {
+        double[] numArray = { 45.3, 67.5, -45.6, 20.34, 33.0, 45.6 };
+        double sum = 0.0;
+
+        for (double num: numArray) {
+            sum += num;
+        }
+
+        double average = sum / numArray.length;
+        System.out.format("The average is: %.2f", average);
+    }
+}
+
