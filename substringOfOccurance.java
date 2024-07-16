46bab9eb9e0d6e172f13625fedf4c2db26589bfb
sriwaugh <sriwaugh0987@gmail.com>
2024-07-16 14:35:20 +0530

    subStringOcurance

---
+++ substringOfOccurance.java
@@ -0,0 +1,15 @@
+package java_program;
+
+public class substringOfOccurance {
+        public static int substringSearch(String str1, String str2) {
+            int index = str1.indexOf(str2);
+            return index != -1 ? index : -1;
+        }
+
+        public static void main(String[] args) {
+            String str1 = "Hello, World!";
+            String str2 = "World";
+            System.out.println(substringSearch(str1, str2));
+        }
+    }
+
