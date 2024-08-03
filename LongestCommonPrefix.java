5396e0440f215502883363a1091481bada5c2a2d
sriwaugh <sriwaugh0987@gmail.com>
2024-08-03 18:10:37 +0530

    LongestCommonPrefix

--- LongestCommonPrefix.java
+++ LongestCommonPrefix.java
@@ -1,5 +1,22 @@
 package java_program;
-
+import java.util.*;
 public class LongestCommonPrefix {
+    public  static String commonprefix(String[] strs){
+    StringBuilder s = new StringBuilder();
+        Arrays.sort(strs);
+    char[] first = strs[0].toCharArray();
+    char[] last = strs[strs.length-1].toCharArray();
+        for(int i=0;i<first.length;i++)
+    {
+        if(first[i]!=last[i])
+            break;
+        s.append(first[i]);
+    }
+        return s.toString();
+}
 
+    public static void main(String[] args) {
+        String[] s={"flower","flow","flight"};
+        commonprefix(s);
+    }
 }
