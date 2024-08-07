1f0b1b371cc658375c65b166985ee8219bfb976e
sriwaugh <sriwaugh0987@gmail.com>
2024-08-07 16:54:23 +0530

    reverseAnArray

---
+++ reverseAnArray.java
@@ -0,0 +1,27 @@
+package java_program;
+
+import java.util.Scanner;
+
+public class reverseAnArray {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n=s.nextInt();
+        int j=n-1;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+        for (int i = 0; i < n ; i++) {
+            if(i<j)
+            {
+               int temp=arr[i];
+               arr[i]=arr[j];
+               arr[j]=temp;
+               j--;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
