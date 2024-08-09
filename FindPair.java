133ffb6144597bc534526803ea5fd8ab7e43162b
sriwaugh <sriwaugh0987@gmail.com>
2024-08-09 10:27:14 +0530

    find pair

--- FindPair.java
+++ FindPair.java
@@ -2,7 +2,6 @@ package java_program;
 
 public class FindPair {
         static void findPair(int arr[], int sum) {
-            int b=0;
             for (int i = 0; i < arr.length - 1; i++) {
                 for (int j = i + 1; j < arr.length; j++) {
                     if (arr[i] + arr[j] == sum) {
