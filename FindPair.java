5e9b1942d53dc234653ffdb8ca70f183a40aecdc
sriwaugh <sriwaugh0987@gmail.com>
2024-08-09 10:32:41 +0530

    find pair

--- FindPair.java
+++ FindPair.java
@@ -5,7 +5,7 @@ public class FindPair {
             for (int i = 0; i < arr.length - 1; i++) {
                 for (int j = i + 1; j < arr.length; j++) {
                     if (arr[i] + arr[j] == sum) {
-                        System.out.println("Pair is found: " + arr[i] + ", " + arr[j]);
+                        System.out.println("Pair  found: " + arr[i] + ", " + arr[j]);
                         return;
                     }
                 }
