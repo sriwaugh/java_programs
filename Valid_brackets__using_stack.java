3c4338b858e9cc5d680a05a4e6ef08a8bb3f5cce
sriwaugh <sriwaugh0987@gmail.com>
2024-08-17 09:52:51 +0530

    valid brackets using stack

--- Valid_brackets__using_stack.java
+++ Valid_brackets__using_stack.java
@@ -4,7 +4,6 @@ import java.util.Stack;
 public class Valid_brackets__using_stack {
         public static void main(String[] args) {
             Scanner s=new Scanner(System.in);
-            int b=0;
             System.out.println("Enter the brackets(Like {}()[])");
             String S=s.next();
             int  n=S.length();
