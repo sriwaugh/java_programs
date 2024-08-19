10541e93bbe2f26dc940444e8cff49915f74903f
sriwaugh <sriwaugh0987@gmail.com>
2024-08-19 22:53:55 +0530

    findTheSecondLargestElement

--- rightRotateOfArray.java
+++ rightRotateOfArray.java
@@ -1,42 +1,40 @@
 package java_program;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class rightRotateOfArray {
-    public static void rightrotate(int[] a,int x)
+    public static void rightrotate(int arr[],int x)
     {
-        int n= a.length;
-        int[] arr =new int[n];
-        int b=0;
-        for (int i = n-1; i >=0 ; i--) {
-            arr[b] = a[i];
-            b++;
+        int n= arr.length;
+        int t[] =new int[n];
+        for (int i = 0; i < n; i++) {
+            t[i]=arr[i];
         }
         for (int i = 0; i < x; i++) {
-            int temp = arr[0];
-            for (int j = 1; j < n; j++) {
-                arr[j - 1] = arr[j];
+            int temp = arr[n-1];
+            for (int j = 0; j < n; j++) {
+                arr[j + 1] = t[j];
             }
-            arr[n - 1] = temp;
+            arr[0] = temp;
         }
-        System.out.println("The "+x+"  left rotated array is ");
-       for (int i = 0; i < n; i++) {
-           System.out.println(arr[i]);
+        System.out.println("The "+x+"  right rotated array is ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
         }
     }
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         System.out.println("Enter the size of the array");
         int n = s.nextInt();
-        int[] a = new int[n];
+        int[] arr = new int[n];
         System.out.println("Enter the array element");
         for (int i = 0; i < n; i++) {
-            a[i] = s.nextInt();
+            arr[i] = s.nextInt();
         }
-        System.out.println("How may time do u want to rotate the array in left");
+        System.out.println("How may time do u want to rotate the array in right");
         int x=s.nextInt();
-        rightrotate(a,x);
+        rightrotate(arr,x);
     }
 }
