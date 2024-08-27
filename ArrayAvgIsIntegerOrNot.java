7047d3007e410e373953613246fd73841ff729a5
sriwaugh <sriwaugh0987@gmail.com>
2024-08-27 11:33:25 +0530

    check the avg is whole number or not

--- ArrayAvgIsIntegerOrNot.java
+++ ArrayAvgIsIntegerOrNot.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ArrayAvgIsIntegerOrNot {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int x=0;
         int n = s.nextInt();
         int arr[] = new int[n];
         for (int i = 0; i < n; i++) {
