88c2710fde27281a2d8cbbb9cbb7f7f4871948eb
sriwaugh <sriwaugh0987@gmail.com>
2024-09-02 21:07:36 +0530

    find duplicate element

--- FindOneDuplicateInArray.java
+++ FindOneDuplicateInArray.java
@@ -17,7 +17,6 @@ public class FindOneDuplicateInArray {
             System.out.println(arr[i]);
         }
     }
-
     public static void main(String[] args) {
         Scanner s= new Scanner(System.in);
         System.out.println("Enter the size of the array");
