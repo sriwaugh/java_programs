9caabc57069782b3a89ad5880040f27a6c899f26
sriwaugh <sriwaugh0987@gmail.com>
2024-09-03 20:40:55 +0530

    Palindrome traingle

--- palindromeTraingle.java
+++ palindromeTraingle.java
@@ -1,6 +1,34 @@
 package java_program;
+import java.util.*;
 
 public class palindromeTraingle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result =new ArrayList<>();
+        if(numRows==0)
+        {
+            return result;
+        }
+        List<Integer> firstrow =new ArrayList<>();
+        firstrow.add(1);
+        result.add(firstrow);
+        if(numRows==1)
+        {
+            return result;
+        }
+        for(int i=1;i<numRows;i++)
+        {
+            List<Integer> prev=result.get(i-1);
+            List<Integer> current=new ArrayList<>();
+            current.add(1);
+            for(int j=1;j<i;j++)
+            {
+                current.add(prev.get(j) + prev.get(j - 1));
+            }
+            current.add(1);
+            result.add(current);
+        }
+        return result;
+    }
     public static void main(String[] args) {
 
     }
