0f51fd5600054819a44bdb7bac29e07f790998b7
sriwaugh <sriwaugh0987@gmail.com>
2024-09-04 20:35:46 +0530

    Longest palindrome substring

--- longestPalindromeSubString.java
+++ longestPalindromeSubString.java
@@ -30,5 +30,7 @@ public class longestPalindromeSubString {
 
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
+        String st=s.nextLine();
+        System.out.println(longestPalindrome(st));
     }
 }
