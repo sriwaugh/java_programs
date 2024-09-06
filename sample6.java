ace0911673e14bad383fa07aa0597e9b31c58410
sriwaugh <sriwaugh0987@gmail.com>
2024-09-06 19:49:40 +0530

    String methods

--- sample6.java
+++ sample6.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 public class sample6 {
     public static void main(String[] args) {
         String sen="The sky is blues";
+        int a=0;
         System.out.println(Arrays.toString(sen.split(" ")));
         String[] sent=sen.split(" ");
         System.out.println(sent[3].length());
