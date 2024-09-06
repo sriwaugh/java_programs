5ca882869f08f5038b741d5b27b1e55ce20ab381
sriwaugh <sriwaugh0987@gmail.com>
2024-09-06 19:47:56 +0530

    sorted array sample

--- sample5.java
+++ sample5.java
@@ -1,7 +1,20 @@
 package java_program;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class sample5 {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int arr[]=new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i]=s.nextInt();
+            arr[i]=arr[i]*arr[i];
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
