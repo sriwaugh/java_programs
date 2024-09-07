1e0dd0a15d96c7026e9484539b721098b989ddfb
sriwaugh <sriwaugh0987@gmail.com>
2024-09-07 19:45:21 +0530

    reverse string sample

--- sample4.java
+++ sample4.java
@@ -2,6 +2,14 @@ package java_program;
 
 public class sample4 {
     public static void main(String[] args) {
+        String sen="The Sky is blue";
+        String[] sent=sen.split(" ");
+        int n=sent.length;
+        for(int i=n-1;i>=0;i--)
 
+        {
+            System.out.print(" "+sent[i]);
+
+        }
     }
 }
