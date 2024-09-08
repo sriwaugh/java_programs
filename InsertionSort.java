4b23a3c255825d4dfb4bad81bf56598204789a0e
sriwaugh <sriwaugh0987@gmail.com>
2024-09-08 19:36:50 +0530

    Insertion sort

--- InsertionSort.java
+++ InsertionSort.java
@@ -23,6 +23,7 @@ public class InsertionSort {
     public static void main(String[] args) {
         int arr[]={10,9,8,7,6,5};
         int n=arr.length;
+        int b=0;
         Insertionsort(arr,n);
     }
 }
